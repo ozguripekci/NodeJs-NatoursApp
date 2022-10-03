d4826cf86f54c3df72140b2bfc8d00062be0c9fe
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-10-03 22:42:28 +0200

    user and tour population on reviewSchema

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -35,7 +35,19 @@ const reviewSchema = new mongoose.Schema({
     toObject: {virtuals:true},
 });
 
+reviewSchema.pre(/^find/, function(next) {
+    this.populate({
+      path :'tour',
+      select : 'name',
+    })
+    .populate({
+        path :'user',
+        select : 'name photo'
+    });
 
-const Review  =  mongoose.model('Review, reviewSchema');
+    next();
+  })
+
+const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
