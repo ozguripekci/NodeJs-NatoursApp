2b9b70dc46e7a9e1b903409e892dce40a34cf1af
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-10-03 23:19:17 +0200

    virtual populate to show reviews in the on of tour

--- models/tourModel.js
+++ models/tourModel.js
@@ -120,6 +120,14 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 })
 
+// Virtual Populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField:'tour',
+  localField: '_id'
+})
+
+
 // Document middleware: runs before .save() command and .create() command
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, {lower:true});
