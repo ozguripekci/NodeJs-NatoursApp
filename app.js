09e183bc96875b563dce74ce234fc7d55c1131da
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-10-08 13:38:32 +0200

    viewsroute, and viewController

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
 const hpp = require('hpp')
+const viewRouter = require('./routes/viewRoutes')
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -71,25 +72,7 @@ app.use((req, res, next) => {
 
 
 // 3) ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour : 'The Forest Hiker',
-    user : 'Özgür'
-  })
-})
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours'
-  })
-})
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
-  })
-})
-
+app.use('/', viewRouter);
 
 
 app.use('/api/v1/tours', tourRouter);
