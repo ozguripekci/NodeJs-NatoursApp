3b52c4d0e1d14d122f4db0518071f7183d75e940
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-10-09 17:34:00 +0200

    login template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -14,7 +14,7 @@ exports.getOverview = catchAsync(async(req, res) => {
     })
 })
 
-exports.getTour = catchAsync(async(req, res) => {
+exports.getTour = catchAsync(async(req, res, next) => {
     // 1)Get the data, for the requested tour (including reviews and guides)
     const tour = await Tour.findOne({slug:req.params.slug}).populate({
         path:'reviews',
@@ -23,8 +23,19 @@ exports.getTour = catchAsync(async(req, res) => {
     // 2) Build template
     // 3) Render that tempalte using tour data from 1)
 
-    res.status(200).render('tour', {
+    res.status(200)
+      .set(
+        'Content-Security-Policy',
+        'connect-src https://*.tiles.mapbox.com https://api.mapbox.com https://events.mapbox.com'
+        )
+      .render('tour', {
       title: `${tour.name} Tour`,
       tour
     })
 })
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+  title: `Log into your account`,
+})
+}
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,50 @@
-
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoib3pndXJpcGVrY2kiLCJhIjoiY2w5MWI5dnkxMTJ0bjNvbXQ2Z3loM3NvbCJ9.x90Vlap-mV8PTidtHr2HvQ';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/ozguripekci/cl91c1b6g008314nrnfsjomwo',
+  scrollZoom:false,
+  // interactive: false
+  // center: [-118.113491, 34.111745],
+  // zoom: 7, 
+
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+    // Create marker
+    const el= document.createElement('div');
+    el.className = 'marker';
+
+    // Add marker
+    new mapboxgl.Marker({
+        element: el,
+        anchor: 'bottom',
+    })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+    // add popup marker
+    new mapboxgl.Popup({
+        offset:30,
+    })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+    // extends map bounds to include current locations
+    bounds.extend(loc.coordinates)
+})
+
+map.fitBounds(bounds, {
+    padding: {
+        top:200,
+        bottom:150,
+        left:100,
+        right:100
+    } 
+});
